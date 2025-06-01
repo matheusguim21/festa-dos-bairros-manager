3cf6e3f5c0b3bdcf17a8a2c87485be5252582b31
matheusguim21 <matheusguim13@gmail.com>
2025-06-01 14:58:26 -0300

    Editing Public Route where redirecets to /preparing if role === ORDER_PREPARER

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -5,9 +5,6 @@ export function PublicRoute() {
   const { user } = useAuth(); // Supondo que useAuth indique se o estado carregou
 
   if (user) {
-    if (user.role === "ORDER_PREPARER") {
-      return <Navigate to="/preparing" replace />;
-    }
     return <Navigate to="/" replace />;
   }
 
